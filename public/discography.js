a4ba82037a0606010b10cf1838b982f8aa4b255b
klia0417 <b2029klialejandro@pshs.edu.ph>
2025-12-11 17:47:44 +0800

    Update discography.js

--- public/discography.js
+++ public/discography.js
@@ -1,19 +1,13 @@
 const albums = {
-  "READY TO BE": {
-    release: "March 10, 2023",
-    tracks: ["SET ME FREE", "MOONLIGHT SUNRISE", "BLAME IT ON ME"]
-  },
-  "BETWEEN 1&2": {
-    release: "August 26, 2022",
-    tracks: ["Talk that Talk", "Queen of Hearts", "Basics"]
-  }
+  "READY TO BE": { release: "March 10, 2023", tracks: ["SET ME FREE", "MOONLIGHT SUNRISE", "BLAME IT ON ME"] },
+  "BETWEEN 1&2": { release: "August 26, 2022", tracks: ["Talk that Talk", "Queen of Hearts", "Basics"] }
 };
 
 function showAlbum(name){
   const album = albums[name];
   if(!album) return;
-  let output = `<h3>${name}</h3><p>Release: ${album.release}</p><ul>`;
+  let output = `<h3>${name}</h3><p><strong>Release Date:</strong> ${album.release}</p><ul>`;
   album.tracks.forEach(track => output += `<li>${track}</li>`);
-  output += '</ul>';
+  output += `</ul>`;
   document.getElementById('albumInfo').innerHTML = output;
 }

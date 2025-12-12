c5eebf391dc0a8ef67befe841ac97fe0deb3cc41
klia0417 <b2029klialejandro@pshs.edu.ph>
2025-12-12 10:24:07 +0800

    Update discography.js

--- public/discography.js
+++ public/discography.js
@@ -1,9 +1,152 @@
 /**
  * discography.js
- * Handles dynamic content display for album tracklists.
+ * Handles dynamic content display for album tracklists, ordered chronologically.
  */
 
 const albums = {
+    // --- MINI ALBUMS (EPs) ---
+    "THE STORY BEGINS": {
+        release: "October 20, 2015",
+        tracks: [
+            "LIKE OOH-AHH (Title Track)",
+            "DO IT AGAIN",
+            "GOING CRAZY",
+            "TRUTH",
+            "CANDY BOY",
+            "LIKE A FOOL"
+        ]
+    },
+    "PAGE TWO": {
+        release: "April 25, 2016",
+        tracks: [
+            "CHEER UP (Title Track)",
+            "PRECIOUS LOVE",
+            "TOUCHDOWN",
+            "READY TO TALK",
+            "TUK TOK",
+            "WOOHOO",
+            "I'M GONNA BE A STAR"
+        ]
+    },
+    "TWICECOASTER: LANE 1": {
+        release: "October 24, 2016",
+        tracks: [
+            "TT (Title Track)",
+            "1 TO 10",
+            "PONYTAIL",
+            "JELLY JELLY",
+            "PIT-A-PAT",
+            "NEXT PAGE",
+            "ONE IN A MILLION"
+        ]
+    },
+    // --- FULL ALBUMS ---
+    "TWICETAGRAM": {
+        release: "October 30, 2017",
+        tracks: [
+            "LIKEY (Title Track)",
+            "TURTLE",
+            "MISSING YOU",
+            "WOW",
+            "FFW",
+            "DING DONG",
+            "REWIND",
+            "LOOK AT ME",
+            "LOVE LINE",
+            "JALJAYO GOOD NIGHT",
+            "ONLY YOU",
+            "24/7",
+            "GREASY"
+        ]
+    },
+    // --- MINI ALBUMS (EPs) ---
+    "WHAT IS LOVE?": {
+        release: "April 9, 2018",
+        tracks: [
+            "WHAT IS LOVE? (Title Track)",
+            "SWEET TALKER",
+            "HO!",
+            "DEAREST MY LOVE",
+            "BE AS ONE (Korean ver.)"
+        ]
+    },
+    "SUMMER NIGHTS": {
+        release: "July 9, 2018",
+        tracks: [
+            "DANCE THE NIGHT AWAY (Title Track)",
+            "CHILLAX",
+            "SHOT THRU THE HEART",
+            "DUCKY",
+            "DEJA VU",
+            "SAY YES"
+        ]
+    },
+    "YES OR YES": {
+        release: "November 5, 2018",
+        tracks: [
+            "YES OR YES (Title Track)",
+            "SAY YOU LOVE ME",
+            "LALALA",
+            "YOUNG & WILD",
+            "SUNSET",
+            "AFTER MOON",
+            "BDZ (Korean ver.)"
+        ]
+    },
+    "FANCY YOU": {
+        release: "April 22, 2019",
+        tracks: [
+            "FANCY (Title Track)",
+            "STUCK IN MY HEAD",
+            "GIRLS LIKE US",
+            "HOT",
+            "TURN IT UP",
+            "STRAWBERRY"
+        ]
+    },
+    "MORE & MORE": {
+        release: "June 1, 2020",
+        tracks: [
+            "MORE & MORE (Title Track)",
+            "OXYGEN",
+            "FIREWORK",
+            "BEGINNING",
+            "DON'T CALL ME AGAIN",
+            "SWEET SUMMER DAY",
+            "SHADOW"
+        ]
+    },
+    // --- FULL ALBUMS ---
+    "EYES WIDE OPEN": {
+        release: "October 26, 2020",
+        tracks: [
+            "I CAN'T STOP ME (Title Track)",
+            "HELL IN HEAVEN",
+            "UP NO MORE",
+            "DO WHAT WE LIKE",
+            "BRING IT BACK",
+            "BELIEVER",
+            "QUEEN",
+            "GO HARD",
+            "SHOT CLOCK",
+            "HANDLE IT",
+            "DEPEND ON YOU",
+            "SAY SOMETHING",
+            "BEHIND THE MASK"
+        ]
+    },
+    // --- MINI ALBUMS (EPs) ---
+    "TASTE OF LOVE": {
+        release: "June 11, 2021",
+        tracks: [
+            "ALCOHOL-FREE (Title Track)",
+            "FIRST TIME",
+            "SCANDAL",
+            "CONGRATULATIONS",
+            "BABY BLUE LOVE",
+            "SOS"
+        ]
+    },
     // --- FULL ALBUMS ---
     "FORMULA OF LOVE": { 
         release: "November 12, 2021", 
@@ -26,54 +169,18 @@ const albums = {
             "SCIENTIST (R3HAB Remix)",
         ] 
     },
-    "EYES WIDE OPEN": {
-        release: "October 26, 2020",
-        tracks: [
-            "I CAN'T STOP ME (Title Track)",
-            "HELL IN HEAVEN",
-            "UP NO MORE",
-            "DO WHAT WE LIKE",
-            "BRING IT BACK",
-            "BELIEVER",
-            "QUEEN",
-            "GO HARD",
-            "SHOT CLOCK",
-            "HANDLE IT",
-            "DEPEND ON YOU",
-            "SAY SOMETHING",
-            "BEHIND THE MASK"
-        ]
-    },
-    "TWICETAGRAM": {
-        release: "October 30, 2017",
-        tracks: [
-            "LIKEY (Title Track)",
-            "TURTLE",
-            "MISSING YOU",
-            "WOW",
-            "FFW",
-            "DING DONG",
-            "REWIND",
-            "LOOK AT ME",
-            "LOVE LINE",
-            "JALJAYO GOOD NIGHT",
-            "ONLY YOU",
-            "24/7",
-            "GREASY"
-        ]
-    },
-
     // --- MINI ALBUMS (EPs) ---
-    "WITH YOU-TH": {
-        release: "February 23, 2024",
+    "BETWEEN 1&2": { 
+        release: "August 26, 2022", 
         tracks: [
-            "ONE SPARK (Title Track)",
-            "I GOT YOU",
-            "RUSH",
-            "NEW NEW",
-            "BLOOM",
-            "YOU GET ME"
-        ]
+            "Talk that Talk (Title Track)", 
+            "Queen of Hearts", 
+            "Basics", 
+            "Trouble", 
+            "Brave", 
+            "Gone", 
+            "When We Were Kids"
+        ] 
     },
     "READY TO BE": { 
         release: "March 10, 2023", 
@@ -87,118 +194,15 @@ const albums = {
             "SET ME FREE (English Version)"
         ] 
     },
-    "BETWEEN 1&2": { 
-        release: "August 26, 2022", 
+    "WITH YOU-TH": {
+        release: "February 23, 2024",
         tracks: [
-            "Talk that Talk (Title Track)", 
-            "Queen of Hearts", 
-            "Basics", 
-            "Trouble", 
-            "Brave", 
-            "Gone", 
-            "When We Were Kids"
-        ] 
-    },
-    "TASTE OF LOVE": {
-        release: "June 11, 2021",
-        tracks: [
-            "ALCOHOL-FREE (Title Track)",
-            "FIRST TIME",
-            "SCANDAL",
-            "CONGRATULATIONS",
-            "BABY BLUE LOVE",
-            "SOS"
-        ]
-    },
-    "MORE & MORE": {
-        release: "June 1, 2020",
-        tracks: [
-            "MORE & MORE (Title Track)",
-            "OXYGEN",
-            "FIREWORK",
-            "BEGINNING",
-            "DON'T CALL ME AGAIN",
-            "SWEET SUMMER DAY",
-            "SHADOW"
-        ]
-    },
-    "FANCY YOU": {
-        release: "April 22, 2019",
-        tracks: [
-            "FANCY (Title Track)",
-            "STUCK IN MY HEAD",
-            "GIRLS LIKE US",
-            "HOT",
-            "TURN IT UP",
-            "STRAWBERRY"
-        ]
-    },
-    "YES OR YES": {
-        release: "November 5, 2018",
-        tracks: [
-            "YES OR YES (Title Track)",
-            "SAY YOU LOVE ME",
-            "LALALA",
-            "YOUNG & WILD",
-            "SUNSET",
-            "AFTER MOON",
-            "BDZ (Korean ver.)"
-        ]
-    },
-    "SUMMER NIGHTS": {
-        release: "July 9, 2018",
-        tracks: [
-            "DANCE THE NIGHT AWAY (Title Track)",
-            "CHILLAX",
-            "SHOT THRU THE HEART",
-            "DUCKY",
-            "DEJA VU",
-            "SAY YES"
-        ]
-    },
-    "WHAT IS LOVE?": {
-        release: "April 9, 2018",
-        tracks: [
-            "WHAT IS LOVE? (Title Track)",
-            "SWEET TALKER",
-            "HO!",
-            "DEAREST MY LOVE",
-            "BE AS ONE (Korean ver.)"
-        ]
-    },
-    "TWICECOASTER: LANE 1": {
-        release: "October 24, 2016",
-        tracks: [
-            "TT (Title Track)",
-            "1 TO 10",
-            "PONYTAIL",
-            "JELLY JELLY",
-            "PIT-A-PAT",
-            "NEXT PAGE",
-            "ONE IN A MILLION"
-        ]
-    },
-    "PAGE TWO": {
-        release: "April 25, 2016",
-        tracks: [
-            "CHEER UP (Title Track)",
-            "PRECIOUS LOVE",
-            "TOUCHDOWN",
-            "READY TO TALK",
-            "TUK TOK",
-            "WOOHOO",
-            "I'M GONNA BE A STAR"
-        ]
-    },
-    "THE STORY BEGINS": {
-        release: "October 20, 2015",
-        tracks: [
-            "LIKE OOH-AHH (Title Track)",
-            "DO IT AGAIN",
-            "GOING CRAZY",
-            "TRUTH",
-            "CANDY BOY",
-            "LIKE A FOOL"
+            "ONE SPARK (Title Track)",
+            "I GOT YOU",
+            "RUSH",
+            "NEW NEW",
+            "BLOOM",
+            "YOU GET ME"
         ]
     }
 };
@@ -226,7 +230,7 @@ function showAlbum(name){
     document.getElementById('albumInfo').innerHTML = output;
 }
 
-// Automatically display the first album (FORMULA OF LOVE) on page load
+// Automatically display the first (oldest) album on page load
 document.addEventListener('DOMContentLoaded', () => {
     const firstAlbumName = Object.keys(albums)[0];
     if (firstAlbumName) {
